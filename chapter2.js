bbadb8942f83a10aa25d68f8a7869999b5ad0bfa
EvgeniiKolesnikov <jekow@yandex.ru>
2021-06-03 01:57:46 +0300

    Add chapter 2.3 Chessboard

--- chapter2.js
+++ chapter2.js
@@ -43,12 +43,44 @@ for (let i = 1; i <= 100; i++) {
 // представляющую сетку 8 х 8, используя для разделения строк символы новой строки. 
 // В каждой позиции сетки стоит либо пробел, либо символ"#". 
 // Эти символы должны располагаться в шахматном порядке. 
+// Передавая данную строку в console. log, вы должны получить что-то вроде
+// этого:
+// # # # #
+// # # # #
+// # # # #
+// # # # #
+// # # # #
+// # # # #
+// # # # #
+// # # # #
 // Если вы уже написали программу, которая генерирует этот узор, определите
 // привязку size = 8 и измените программу так, чтобы она работала для любого
 // size, выводя сетку заданных ширины и высоты. 
 console.log('=== Chapter 2.3 Шахматная доска');
-function createChessboard(size) { 
-  console.log(size);
+function createChessboard(size, cellA, cellB) {
+  console.log(`Chessboard size = ${size}`);
+  if (cellA === undefined) cellA = ' ';
+  if (cellB === undefined) cellB = '#';
+  for (let i = 0; i < size; i++) {
+    if (i % 2 === 0) {
+      createLine(cellA, cellB, size);
+    } else {
+      createLine(cellB, cellA, size);
+    }
+  }
+  function createLine(cellA, cellB, size) {
+    let line = '';
+    for (let i = 0; i < size; i++) {
+      if (i % 2 === 0) {
+        line += cellA;
+      } else {
+        line += cellB;
+      }
+    }
+    console.log(line);
+  }
 }
 createChessboard(8);
+createChessboard(4, '☐', '☒');
+createChessboard(3, '-', '•');
 //#endregion
