06bc84539f9247bcb4533f2e64c996459e5dfbf2
EvgeniiKolesnikov <jekow@yandex.ru>
2021-06-03 22:10:53 +0300

    Add chapter 3.2

--- chapter3.js
+++ chapter3.js
@@ -17,10 +17,32 @@ console.log(`min = ${min(1,2,3,0,-5,13)}`);
 //#endregion
 
 //#region Chapter 3.2 Рекурсия
+// ноль четный;
+// единица нечетная;
+// четность любого другого числа N совпадает с четностью N - 2.
+
+// Определите рекурсивную функцию isEven, соответствующую этому описанию. 
+// Функция должна принимать один параметр (положительное целое число) 
+// и возвращать логическое значение.
+// Проверьте эту функцию на числах 50 и 75. Посмотрите, как она ведет себя
+// для -1. Почему? Можете ли вы придумать способ, как это исправить?
 console.log('=== Chapter 3.2 Рекурсия');
 function isEven(n) {
-
+  // console.log(n);
+  if (n === 0) return true;
+  if (n === 1) return false;
+  if (n>0) {
+    return isEven(n-2);
+  } else {
+    return isEven(n+2);
+  }
 }
+console.log(isEven(50));      // → true
+console.log(isEven(75));      // → false
+console.log(isEven(1));       // → false
+console.log(isEven(0));       // → true   
+console.log(isEven(-1));      // → false
+console.log(isEven(-2));      // → true 
 //#endregion
 
 //#region Chapter 3.3 Подсчет букв 
