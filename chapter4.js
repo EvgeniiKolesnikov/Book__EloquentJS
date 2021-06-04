0eb0f662158d3ca2c723c867413274ac95ce0d99
EvgeniiKolesnikov <jekow@yandex.ru>
2021-06-05 00:27:44 +0300

    Add chapter 4.2

--- chapter4.js
+++ chapter4.js
@@ -18,12 +18,14 @@ console.log('Chapter 4. Excercises');
 console.log('=== Chapter 4.1 Сумма диапазона');
 function range(start, end, step = 1) {
   let array = [];
+  if (step === 0) return array;
   if (start <= end) {
+    if (step < 0) step *= -1;
     for (let i = start; i <= end; i += step) {
       array.push(i);
     }
   } else {
-    if (step >= 0) step *= -1;
+    if (step > 0) step *= -1;
     for (let i = start; i >= end; i += step) {
       array.push(i);
     }
@@ -40,16 +42,46 @@ console.log(range(7, 2));                     // [7, 6, 5, 4, 3, 2]
 console.log(range(1, 10, 2));                 // [1, 3, 5, 7, 9]
 console.log(range(10, 1, -2));                // [10, 8, 6, 4, 2]
 
+console.log(range(1, 2, 0));                  // []
+console.log(range(1, 11, -2.5));              // [1, 3.5, 6, 8.5, 11]
+
 console.log(sum(range(1, 10)));               // 55
 console.log(sum(range(1, 10, 1)));            // 55
 console.log(sum(range(7, -6)));               // 7  
 //#endregion
 
-//#region Chapter 4.2 
-console.log('=== Chapter 4.2 ');
+//#region Chapter 4.2 Массив в обратном порядке
+// Напишите две функции: reverseArray и reverseArrayInPlace. 
+// Первая функция, reverseArray, принимает массив в качестве аргумента 
+// и создает новый массив, содержащий те же элементы в обратном порядке. 
+// Вторая, reverseArrayInPlace, делает то же, что и метод reverse: 
+// преобразовывает массив, заданный в качестве аргумента, меняя порядок 
+// следования его элементов на обратный. Не используйте для этого
+// стандартный метод reverse. 
+console.log('=== Chapter 4.2 Массив в обратном порядке');
+function reverseArray(arr) {
+  let reverseArray = [];
+  arr.map(item => reverseArray.unshift(item));
+  return reverseArray;
+}
+function reverseArrayInPlace(arr) {
+  let reverseArray = [];
+  arr.map(item => reverseArray.unshift(item));
+  for (i in arr) {
+    arr[i] = reverseArray[i];
+  }
+}
+
+let array = [1, 2, 3, 4, 5];
+
+console.log(reverseArray(["A", "B", "C"]));             // ["C", "B", "A"]
+console.log('array = ', array);                         // [1, 2, 3, 4, 5]
+console.log('reverseArray = ', reverseArray(array));    // [5, 4, 3, 2, 1]
+reverseArrayInPlace(array);
+console.log('reverseArrayInPlace array = ', array);     // [5, 4, 3, 2, 1]
 //#endregion
 
-//#region Chapter 4.3 
+//#region Chapter 4.3
 console.log('=== Chapter 4.3 ');
 //#endregion
 
