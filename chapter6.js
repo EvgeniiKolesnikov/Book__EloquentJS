d53eee503b04adbaf464a8b33e9a3e2b5b6c5214
EvgeniiKolesnikov <jekow@yandex.ru>
2021-06-10 02:31:34 +0300

    Add chapter 6.3

--- chapter6.js
+++ chapter6.js
@@ -112,6 +112,9 @@ class Group {
     arr.map(value => group.add(value));
     return group;
   }
+  [Symbol.iterator]() {
+    return new GroupIterator(this);
+  }
 }
 
 let group = Group.from([0, 5, 10, 15, 20, 25]);
@@ -127,7 +130,37 @@ console.log('group = ', group);     // group: (5) [0, 5, 15, 20, 25]
 //#endregion
 
 //#region Chapter 6.3 Итерируемые группы
+// Сделайте класс Group из предыдущего упражнения итерируемым. Если вы
+// не помните точный вид интерфейса итератора, перечитайте раздел, 
+// посвященный этому интерфейсу, ранее в данной главе.
+// Если для представления членов группы вы использовали массив, 
+// не возвращайте просто итератор, созданный путем вызова метода 
+// Symbol.iterator для массива. Это бы сработало, но оно не 
+// соответствует цели данного упражнения. 
+// Если ваш итератор ведет себя странно, когда группа изменяется во время
+// итерации, - это нормально. 
 console.log('=== Chapter 6.3 Итерируемые группы');
+class GroupIterator {
+  constructor(group) {
+    this.group = group;
+    this.index = 0;
+  }
+  next() {
+    if (this.index >= this.group.group.length) return {done: true};
+    else {
+      let result = {value: this.group.group[this.index], done: false};
+      this.index++;
+      return result;
+    }
+  }
+}
+
+for (let value of Group.from(["a", "b", "c"])) {
+  console.log(value);
+}
+// → a
+// → b
+// → c
 //#endregion
 
 //#region Chapter 6.4 Заимствование метода
