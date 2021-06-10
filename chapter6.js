8e49b96f22e666791a6417f66b01b336e23d4fa5
EvgeniiKolesnikov <jekow@yandex.ru>
2021-06-10 03:41:41 +0300

    Add chapter 6.5

--- chapter6.js
+++ chapter6.js
@@ -176,3 +176,66 @@ let map = {one: true, two: true, hasOwnProperty: true};
 // console.log(map.hasOwnProperty("one"));
 console.log(Object.prototype.hasOwnProperty.call(map, "one"));    // → true
 //#endregion
+
+//#region Chapter 6.5 Интерфейс к последовательностям (134 old book)
+// Разработайте интерфейс, абстрагирующий проход по набору значений.
+// Объект с таким интерфейсом представляет собой последовательность, а
+// интерфейс должен давать возможность в коде проходить по
+// последовательности, работать со значениями, которые её составляют, 
+// и както сигнализировать о том, что мы достигли конца последовательности.
+// Задав интерфейс, попробуйте сделать функцию logFive, которая принимает
+// объект-последовательность и вызывает console.log для первых её пяти
+// элементов – или для меньшего количества, если их меньше пяти.
+// Затем создайте тип объекта ArraySeq, оборачивающий массив, и
+// позволяющий проход по массиву с использованием разработанного вами
+// интерфейса. Создайте другой тип объекта, RangeSeq, который проходит по
+// диапазону чисел (его конструктор должен принимать аргументы from и to).
+console.log('=== Chapter 6.5 Интерфейс к последовательностям');
+function ArraySeq(data) {
+  this.position = -1;
+  this.data = data;
+}
+ArraySeq.prototype.next = function () {
+  if (this.position >= this.data.length - 1) {
+    return false;
+  }
+  this.position++;
+  return true;
+}
+ArraySeq.prototype.curr = function () {
+  return this.data[this.position];
+} 
+
+function RangeSeq(from, to) {
+  this.position = from - 1;
+  this.to = to;
+}
+RangeSeq.prototype.next = function () {
+  if (this.position >= this.to) {
+    return false;
+  }
+  this.position++;
+  return true;
+}
+RangeSeq.prototype.curr = function () {
+  return this.position;
+} 
+function logFive(seq) {
+  for (var i = 0; i < 5; i++) {
+    if (!seq.next()) {
+      break;
+    }
+    console.log(seq.curr()); 
+  }
+}
+
+logFive(new ArraySeq([1, 2]));
+// → 1
+// → 2
+logFive(new RangeSeq(100, 1000));
+// → 100
+// → 101
+// → 102
+// → 103
+// → 104
+//#endregion
