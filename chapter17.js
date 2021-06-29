672eb935833fca6dab3ad5a2b35b0895fbe16968
EvgeniiKolesnikov <jekow@yandex.ru>
2021-06-29 03:39:55 +0300

    Add Chapter 17.3

--- chapter17.js
+++ chapter17.js
@@ -128,7 +128,7 @@ createRoundDiagram(300, 150, 100);
 // с постоянной скоростью и отскакивает от стенок прямоугольника, когда
 // касается их. 
 console.log('=== Chapter 17.3 Прыгающий шарик');
-let cx3 = document.querySelector("#canvas3").getContext("2d");
+let cx = document.querySelector("#canvas3").getContext("2d");
 let lastTime = null;
 
 function frame(time) {
@@ -140,7 +140,24 @@ function frame(time) {
 }
 requestAnimationFrame(frame);
 
+let x = 100, y = 300;
+let radius = 10;
+let speedX = 100, speedY = 60;
+
 function updateAnimation(step) {
-  // Your code here.
+  // Default code below 
+  cx.clearRect(0, 0, 400, 400);
+  cx.strokeStyle = "blue";
+  cx.lineWidth = 4;
+  cx.strokeRect(25, 25, 350, 350);
+  
+  x += step * speedX;
+  y += step * speedY;
+  if (x < 25 + radius || x > 375 - radius) speedX = -speedX;
+  if (y < 25 + radius || y > 375 - radius) speedY = -speedY;
+  cx.fillStyle = "red";
+  cx.beginPath();
+  cx.arc(x, y, radius, 0, 7);
+  cx.fill();
 }
 //#endregion
